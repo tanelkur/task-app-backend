e7581b3ec89c1ba7102aea16a5d930460a72c78f
tanelkur <106887540+tanelkur@users.noreply.github.com>
2023-05-15 15:42:17 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose
   .then(() => {
     console.log("mongoDB connected");
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
